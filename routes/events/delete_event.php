2c8182b9524635bba83bf2f71a6c4aed92ef51c6
zrek87 <93806801+zrek87@users.noreply.github.com>
2025-05-11 20:52:51 +0300

    Update delete_event.php

--- routes/events/delete_event.php
+++ routes/events/delete_event.php
@@ -2,10 +2,12 @@
 require_once "../../includes/conn.php"; 
 require_once "../../includes/functions.php"; 
 
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://revento.mhzrek.com");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json");
+
 
 $data = json_decode(file_get_contents("php://input"), true);
 $event_id = $data['event_id'] ?? null;
