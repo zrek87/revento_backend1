2de1d02793d7b55b4dcbd548dca64709e3da6de4
zrek87 <93806801+zrek87@users.noreply.github.com>
2025-05-11 16:33:02 +0300

    Update update_preferences.php

--- routes/auth/update_preferences.php
+++ routes/auth/update_preferences.php
@@ -4,7 +4,7 @@ include('../../includes/conn.php');
 include('../../includes/functions.php');
 
 // Static CORS for deployed frontend
-header("Access-Control-Allow-Origin: http://ckkso0s04080wkgskwkowwso.217.65.145.182.sslip.io");
+header("Access-Control-Allow-Origin: https://revento.mhzrek.com");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
