90556e951353f84b1b5b602250381fbd6f8dc6bf
zrek87 <93806801+zrek87@users.noreply.github.com>
2025-05-11 20:52:11 +0300

    Update add_event.php

--- routes/events/add_event.php
+++ routes/events/add_event.php
@@ -2,10 +2,11 @@
 require_once "../../includes/conn.php";
 require_once "../../includes/functions.php";
 
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://revento.mhzrek.com");
 header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     sendJsonResponse(false, "Invalid request method.");
